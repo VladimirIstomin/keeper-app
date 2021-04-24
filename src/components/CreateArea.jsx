cb662d27108cd8d0c4be689dea159d856afd2aef
Vladimir Istomin <istomin2798@gmail.com>
2021-04-24 17:05:21 +0300

    Add a little of styling

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import React, {useState} from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+  const [isExpandedInput, setIsExpandedInput] = useState(false);
+
   const [noteItem, setNoteItem] = useState(
     {
       title: '',
@@ -25,15 +30,21 @@ function CreateArea(props) {
       title: '',
       content: ''
     });
+    setIsExpandedInput(false);
   }
 
 
   return (
     <div>
-      <form>
-        <input name="title" onChange={updateNoteItem} value={noteItem.title} placeholder="Title" />
-        <textarea name="content" onChange={updateNoteItem} value={noteItem.content} placeholder="Take a note..." rows="3" />
-        <button onClick={addNoteMiddleware}>Add</button>
+      <form className="create-note">
+        {isExpandedInput && <input name="title" onChange={updateNoteItem} value={noteItem.title} placeholder="Title" />}
+        <textarea
+          name="content"
+          onChange={updateNoteItem}
+          onClick={event => setIsExpandedInput(true)}
+          value={noteItem.content}
+          placeholder="Take a note..." rows={isExpandedInput ? 3 : 1} />
+        <Zoom in={isExpandedInput}><Fab onClick={addNoteMiddleware}><AddIcon /></Fab></Zoom>
       </form>
     </div>
   );
